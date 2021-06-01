b87dbeeeb6d79946f145f156ba4d61681183800b
08jhs05 <08jhs05@gmail.com>
2021-05-31 20:08:21 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from "react";
+import dva from './dva.jpg';
 import './App.css';
+import "@tensorflow/tfjs";
+const posenet = require('@tensorflow-models/posenet');
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+
+  componentDidMount() {
+    async function estimatePoseOnImage(imageElement) {
+      // load the posenet model from a checkpoint
+      const net = await posenet.load();
+
+      const pose = await net.estimateSinglePose(imageElement, {
+        flipHorizontal: false
+      });
+      return pose;
+    }
+    const imageElement = document.getElementById('dva');
+
+    const pose = estimatePoseOnImage(imageElement);
+
+    pose.then( (res) => {
+      console.log(res);
+    });
+  }
+  
+  render() {
+    return(<div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={dva} alt="logo" id="dva"/>
       </header>
-    </div>
-  );
+    </div>);
+  }
 }
 
 export default App;
